74cb23cf15ebaeb93c2fe1e652713ab082ffb6c7
Jake Edgar <jakeedgar1012@gmail.com>
2022-03-30 11:52:07 -0700

    passed first test to check if input is a vowel
    Co-authored-by: Caleb Coughenour <CalebCoughenour@protonmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,27 @@ function filterPuncuation(word) {
 
 // BUSINESS LOGIC 
 
-function vowelCounter(text) {
-  return text.split("").includes(function(vowel) {
-    return "aeiou".includes(vowel);
-  }).join("");
+const vowelArray = ["a", "e", "i", "o", "u"]
+
+function vowelChecker(text) {
+  const vowelArray = ["a", "e", "i", "o", "u"]
+  let splitText = text.split();
+  for (let i = 0; i <= vowelArray.length; i ++) {
+    if (splitText[0].toLowerCase("a") === vowelArray[i]) {
+      return true;
+    };
+  return false;
+  }
 }
+
+
+// function vowelCounter(text, vowel) {
+//   for (let i = 0; i <= text.length; i +=1) {
+//     if (text.includes[i] === vowel) {
+//       return true;
+//     };
+//   }
+//   return false;
+// }
 
 // UI LOGIC
